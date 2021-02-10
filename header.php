d06b905524bb866ac8e273b8244488083ff308e6
NumbShell <acxey32@gmail.com>
2021-02-10 21:53:41 +0100

    Remake of theme

--- header.php
+++ header.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<link rel="stylesheet" href="<?php echo 'css/bootstrap.min.css'; ?>">
+	<link rel="stylesheet" href="<?php echo 'css/style.css'; ?>">
+</head>
 <body>
-    <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">WordPress Theme</span>
-        </div>
-    </nav>
+    <div id="navbar" class="row justify-content-md-center">
+        <nav class="navbar navbar-expand-lg navbar-bg navbar-fixed-top p-3">
+          <div class="container-fluid">
+            <a class="navbar-brand navbar-title" href="#">NEWS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto w-100 justify-content-center navbar-nav-colors">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Recent News</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Politics</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Covid-19</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">U.S.News</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Opinion</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Business</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">World</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,49 +7,142 @@
 	</head>
 	<body>
 		<div class="container-fluid">
-			<div class="row mb-5">
 			<?php include('header.php'); ?>
+			<div class="row mb-5">
 			</div>
 			<div class="container">
 				<div class="row justify-content-md-center">
-					<div class="col-md-6 text-left">
-						<h1 id="title">Privacy infrastructure trusted by top teams</h1>
-						<p class="text-note">Transcend is the data privacy infrastructure that makes it simple to give your users control over their personal data.</p>
-						<form class="d-flex">
-							<div class="col-auto d-flex">
-								<input class="form-control me-2" type="search" placeholder="Your work email" aria-label="Your work email">
+					<div class="col-md-12 text-center">
+						<h1 id="title">Covid lab leak in China 'extremely unlikely' - WHO</h1>
+				</div>
+				<div class="col-md-12 m-2"></div>
+					<div class="row">
+						<div class="col-md-3">
+							<div class="row">
+								<div class="col-md-3 fill">
+									<img src="<?php echo 'img/thumbnail_02.png'; ?>" alt="Image">
+								</div>
+								<div class="col-md-9">
+									<a href="#"><p>Boston Dynamics sold for $1 billion to Google.</p></a>
+								</div>
 							</div>
-							<div class="col-auto">
-								<button type="submit" class="btn btn-link">Get a demo</button>
+							<div class="row">
+								<div class="col-md-3 fill">
+									<img src="<?php echo 'img/thumbnail_01.png'; ?>" alt="Image">
+								</div>
+								<div class="col-md-9">
+									<a href="#"><p>Covid-19 Moderna vaccine is now available to the public after clinical trials.</p></a>
+								</div>
 							</div>
-						</form>
-						<div class="mb-4"></div>
-						<div class="row d-flex flex-wrap align-items-center justify-content-md-center">
-							<div class="col-md-4">
-								<a href="#"><img src="<?php echo 'img/indiegogo.png'; ?>" class="img-fluid" alt="..."></a>
+							<div class="row">
+								<div class="col-md-3 fill">
+									<img src="<?php echo 'img/thumbnail_03.png'; ?>" alt="Image">
+								</div>
+								<div class="col-md-9">
+									<a href="#"><p>Deepmind new AI can now play hide and seek.</p></a>
+								</div>
 							</div>
-							<div class="col-md-4">
-								<a href="#"><img src="<?php echo 'img/opendoor.png'; ?>" class="img-fluid" alt="..."></a>
-							</div>
-							<div class="col-md-4">
-								<a href="#"><img src="<?php echo 'img/ethos.png'; ?>" class="img-fluid" alt="..."></a>
-							</div>
-							<div class="col-md-4">
-								<a href="#"><img src="<?php echo 'img/patreon.png'; ?>" class="img-fluid" alt="..."></a>
-							</div>
-							<div class="col-md-4">
-								<a href="#"><img src="<?php echo 'img/robinhood.png'; ?>" class="img-fluid" alt="..."></a>
-							</div>
-							<div class="col-md-4">
-								<a href="#"><img src="<?php echo 'img/athletic.png'; ?>" class="img-fluid" alt="..."></a>
-							</div>
+							<div class="row">
+								<div class="col-md-3 fill">
+									<img src="<?php echo 'img/thumbnail_04.png'; ?>" alt="Image">
+								</div>
+								<div class="col-md-9">
+									<a href="#"><p>Vertical farming might be a solution for climate.</p></a>
+								</div>
 						</div>
-						<p class="text-note">We're simplifying privacy for engineers and lawyers at brands you love.</p>
 					</div>
-					<div class="col-md-6">
-						<!--<img src="img/coffee.jpg" class="img-fluid" alt="...">-->
+					<div class="col-md-6 text-center">
+						<div class="col img-display">
+							<img src="<?php echo 'img/thumbnail_05.png'; ?>" alt="Image" class="img-fluid">
+						</div>
+						<p>Special coverage of Donald Trump's second Senate impeachment trial</p>
+					</div>
+					<div class="col-md-3">
+						<h5>Day Two of Trump's Senate trial</h5>
+						<ul class="list-group list-group-flush">
+						  <li class="">
+						  <a href="#">
+						  	<span id="time-newest" class="time-text">2m ago</span>
+							<p>Trial resumes</p>
+						  </a>
+						  </li>
+						  <li class="">
+							<a href="#">
+								<span class="time-text">8m ago</span>
+								<p>Hawley, other GOP senators seem distracted during key arguments</p>
+							</a>
+						  </li>
+						  <li class="">
+							<a href="#">
+								<span class="time-text">12m ago</span>
+								<p>Trial breaks for 15 minutes</p>
+							</a>
+						  </li>
+						  <li class="">
+							<a href="#">
+								<span class="time-text">27m ago</span>
+								<p>Dean: Trump 'relentless' about efforts to overturn election</p>
+							</a>
+						  </li>
+						</ul>
 					</div>
 				</div>
+				<div class="m-4"></div>
+				<!--
+				<div class="row">
+					<div class="col-md-4">
+						<h5>Top stories</h5>
+						<div class="row">
+							<div class="col-md-3 fill">
+								<img src="<?php echo 'img/thumbnail_02.png'; ?>" alt="Image">
+							</div>
+							<div class="col-md-9">
+								<p>Boston Dynamics sold for $1 billion to Google.</p>
+							</div>
+						</div>
+						<div class="m-3"></div>
+						<div class="row">
+							<div class="col-md-3 fill">
+								<img src="<?php echo 'img/thumbnail_01.png'; ?>" alt="Image">
+							</div>
+							<div class="col-md-9">
+								<p>Covid-19 Moderna vaccine is now available to the public after clinical trials prove it safe.</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-3 fill">
+								<img src="<?php echo 'img/thumbnail_03.png'; ?>" alt="Image">
+							</div>
+							<div class="col-md-9">
+								<p>Deepmind new AI can now play hide and seek.</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-3 fill">
+								<img src="<?php echo 'img/thumbnail_04.png'; ?>" alt="Image">
+							</div>
+							<div class="col-md-9">
+								<p>Vertical farming might be a solution.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<h5>More news</h5>
+						<div class="row">
+							<div class="col-md-3 fill">
+								<img src="<?php echo 'img/thumbnail_02.png'; ?>" alt="Image">
+							</div>
+							<div class="col-md-9">
+								<p>Boston Dynamics sold for $1 billion to Google.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<h5>THINK: Opinion, analysis, essays</h5>
+					</div>
+				</div>
+				-->
+			</div>
 		</div>
 	</body>
 </html>
